56ea86ba8d8188d6dfe9c6fe8144765ab0430adb
Zachary Mollenhour <zacharymollenhour@Zacharys-MacBook-Air.local>
2021-01-11 03:52:55 -0500

    Easy FizzBuzz question

---
+++ FizzBuzz.py
@@ -0,0 +1,11 @@
+#Write a program that prints the numbers from 1 to 20. But for multiples of three print “Fizz” instead of the number and for the multiples of five print “Buzz”. For numbers which are multiples of both three and five print “FizzBuzz”.
+#Basic FizzBuzz type question. No overreaching question
+for num in range(1,21):
+    string = ""
+    if num % 3 == 0:
+        string = string + "Fizz"
+    if num % 5 == 0:
+        string = string + "Buzz"
+    if num % 5 != 0 and num % 3 != 0:
+        string = string + str(num)
+    print(string)
