79876ab1ebb942844e766108d5b41e1beec81a03
Zachary Mollenhour <zacharymollenhour@Zacharys-MacBook-Air.local>
2021-01-11 03:11:42 -0500

    Log Parsing

--- LogParsing.py
+++ LogParsing.py
@@ -3,6 +3,14 @@ import re
 
 #Start by opening the log file
 log_file_path = r"./test_log.log"
-with open(log_file_path) as file:
+
+#Regex to match for css files
+regex = re.compile(r'.*path="/.......css')
+
+#Open Log file and search for above regex
+with open(log_file_path,"r") as file:
     for line in file:
-        print(line)
+        if (regex.search(line)):
+            print(line)
+            
+        
